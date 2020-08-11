dbca4f7983074a17f7f72362854dc7fa47b03609
make2019 <jfferreira1993@gmail.com>
2020-08-11 15:48:35 -0300

    Equo Toolbar Api and Equo Toolbar Wc was added as node-package Projects in com.make.equo.node.packages.

---
+++ com.make.equo.node.packages/packages/equo-toolbar-wc/src/components/Toolbar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div data-app class="toolbar-container">
+        <v-toolbar height="35px" :color="color">
+          <slot></slot>
+          <v-spacer></v-spacer>
+          <v-btn dark icon text>
+          <font-awesome-icon :icon="['fas','times']" />
+        </v-btn>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {VToolbar, VBtn, VSpacer} from 'vuetify/lib'
+
+library.add(
+  fas
+)
+
+export default {
+  name: 'equo-toolbar',
+  components: { FontAwesomeIcon, VToolbar, VBtn, VSpacer},
+  props: {
+    color: {
+      type: String,
+      default: "#07F"
+    }
+  },
+  data: () => ({
+     
+  }),
+    methods: {
+    }
+
+  }
+</script>
+<style>
+.toolbar-container{
+  height: 35px;
+}
+.v-toolbar__content, .v-toolbar__extension {
+    padding: 0px 0px;
+}
+@import url("./../styles/vuetify.css");
+@import url("./../styles/styles.css");
+
+</style>
---
+++ com.make.equo.node.packages/packages/equo-toolbar-wc/src/components/Toolitem.vue
@@ -0,0 +1,56 @@
+
+<template>
+    <v-tooltip data-app bottom>
+            <template v-slot:activator="{ on }">
+                <v-btn v-on:click="eventhandlerEval" icon text dark v-on="on">
+                  <font-awesome-icon :icon="['fas',icon]" />
+                </v-btn>
+            </template>
+            <span>{{tooltip}}</span>
+    </v-tooltip>
+</template>
+
+
+<script>
+import { VBtn, VTooltip } from 'vuetify/lib'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+library.add(
+  fas
+)
+
+  export default {
+    name: "equo-toolitem",
+    components: { VBtn, VTooltip, FontAwesomeIcon },
+        props:{
+        eventhandler:{
+            type: String,
+            default: (()=>{console.warn("On click event Listener by default, please define an EventHandler for each Equo ToolItem");}).toString(),
+        },
+        tooltip:{
+            type: String,
+            default: "Tooltip"
+        },
+        icon:{
+            type: String,
+            default: "info-circle"
+        }
+    },
+    methods: {
+      eventhandlerEval(){
+        var f = new Function("return " + this.eventhandler.toString())();
+        f();
+      } 
+    }
+  }
+</script>
+<style>
+.v-tooltip{
+  display: block;
+}
+
+@import url("./../styles/vuetify.css");
+@import url("./../styles/styles.css");
+</style>

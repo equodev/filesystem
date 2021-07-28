b2a8a6f7a9a3e7ad0c25e444798c2ec2eb93ef3b
mauro <mauropuravida@gmail.com>
2021-07-28 15:30:47 -0300

    fix: Support overflow for buttons in the toolbar

--- com.equo.node.packages/packages/equo-toolbar-wc/src/components/Toolbar.vue
+++ com.equo.node.packages/packages/equo-toolbar-wc/src/components/Toolbar.vue
@@ -1,16 +1,36 @@
 <template>
-  <b-navbar class="toolbar-container " :style="getBgColor">
-    <slot></slot>
-  </b-navbar>
+  <div class="toolbar-container" :style="getBgColor">
+    <b-navbar class="toolbar-nav">
+      <slot></slot>
+    </b-navbar>
+    <equo-toolitem :tooltip="closeTooltip" :icon="closeIcon" :visible="closeVisible" :commevent="closeEvent" class="close-item"></equo-toolitem>
+  </div>
 </template>
 
 <script>
+import EquoToolitem from '../components/Toolitem.vue';
 export default {
   name: 'equo-toolbar',
+  components: {EquoToolitem},
   props: {
     toolbarColor: {
       type: String,
       default: "#007bff"
+    },
+    closeTooltip: {
+      type: String,
+      default: "Tooltip"
+    },
+    closeIcon: {
+      type: String,
+      default:""
+    },
+    closeVisible: {
+      type: String,
+      default: "false"
+    },
+    closeEvent: {
+      type: String
     }
   },
   computed: {
@@ -22,12 +42,22 @@ export default {
 
 </script>
 <style>
+.toolbar-nav,
 .toolbar-container {
   display: flex;
-  height: 35px;
+  min-height: 35px;
   padding-left: 5px;
   padding-right: 5px;
   align-items: center;
+}
+
+.toolbar-nav {
+  flex-wrap: wrap;
+}
+
+.close-item {
+  margin-left: auto;
+  align-self: normal;
 }
 
 </style>
